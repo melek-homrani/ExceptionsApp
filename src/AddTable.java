8179b1a27fbe28190c17720b1dc37100cd6ae81f
melekhomrani <melekhomrani87@gmail.com>
2023-10-06 14:24:45 +0100

    ArrayIndexOutOfBoundsException

--- src/AddTable.java
+++ src/AddTable.java
@@ -6,18 +6,21 @@
 import java.util.Arrays;
 
 /**
- *
  * @author Melek Homrani
  */
 public class AddTable {
     public static void main(String[] args) {
 
-		int[] array = new int[3];
-		for(int i=0;i<4;++i){
-			array[i] = i;
-		}
-		System.out.println(Arrays.toString(array));
-	}
+        int[] array = new int[3];
+        for (int i = 0; i < 4; ++i) {
+            array[i] = i;
+        }
+        try {
+            System.out.println(Arrays.toString(array));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is out of bounds exception.");
+        }
+    }
 }
 
 
