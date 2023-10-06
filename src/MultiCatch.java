2208cb2f40c4415d734ed15c8d61f87c12c86989
melekhomrani <melekhomrani87@gmail.com>
2023-10-06 14:39:52 +0100

    RuntimeException

--- src/MultiCatch.java
+++ src/MultiCatch.java
@@ -11,7 +11,7 @@ class MultiCatch {
             System.out.println("No Of Arguments = " + a);
             int b = 42 / a;//Could generate Divide by Zero exception.
 
-            int c[] = {1}; //array initialized with size 1
+            int[] c = {1}; //array initialized with size 1
             c[42] = 99;//Generate Array Out Of Bound exception.
         } catch (RuntimeException e) {
             if(e instanceof ArithmeticException)
