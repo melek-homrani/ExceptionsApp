d03f634bf658dbf6548736e0dcb93f1583b64940
melekhomrani <melekhomrani87@gmail.com>
2023-10-06 14:34:13 +0100

    IOException

--- src/TestFinally.java
+++ src/TestFinally.java
@@ -1,20 +1,21 @@
+import java.io.IOException;
 
-
-
-public class TestFinally{
-    
- 
+public class TestFinally {
 
     public static void main(String[] args) {
-     
+
+        try {
             System.out.println("entrer votre code ");
-            // traiter cette exception 
-          int x =   System.in.read();
+            // traiter cette exception
+            int x = System.in.read();
 
-     
+        } catch (IOException e) {
+            System.out.println("Merci de votre visite");
+        } finally {
+            // afficher toujours le message "Merci de votre visite"
+            System.out.println("Merci de votre visite");
+        }
 
-        // afficher toujours le message "Merci de votre visite"
-     
     }
 
 }
