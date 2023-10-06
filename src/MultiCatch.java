b30b09ffd92b66ffd5e9e4d485d0baf550f90a50
melekhomrani <melekhomrani87@gmail.com>
2023-10-06 14:39:23 +0100

    RuntimeException

--- src/MultiCatch.java
+++ src/MultiCatch.java
@@ -3,26 +3,24 @@
 import java.io.*;
 import java.lang.*;
 
-class MultiCatch{
-    public static void main (String args[]){
-      try{
+class MultiCatch {
+    public static void main(String[] args) {
+        try {
 
-   		int a = args.length;
-   		System.out.println("No Of Arguments = " +  a);
-   		int b =  42 / a;//Could generate Divide by Zero exception.
+            int a = args.length;
+            System.out.println("No Of Arguments = " + a);
+            int b = 42 / a;//Could generate Divide by Zero exception.
 
-   		int c[ ] = { 1 }; //array initialized with size 1
-   		c[42] = 99;//Generate Array Out Of Bound exception.
-      }
-      catch (RuntimeException e){
-           System.out.println("Array index is out of bounds exception.");
-   	  }
-      catch (ArithmeticException e){
-           System.out.println("Divide by 0 exception." );
-      }
-   }
+            int c[] = {1}; //array initialized with size 1
+            c[42] = 99;//Generate Array Out Of Bound exception.
+        } catch (RuntimeException e) {
+            if(e instanceof ArithmeticException)
+                System.out.println("Cannot divide by 0.");
+            else if(e instanceof ArrayIndexOutOfBoundsException)
+                System.out.println("Array index out-of-bounds.");
+            else
+                System.out.println("Unknown exception.");
+        }
+    }
 
 }
-
-
-
